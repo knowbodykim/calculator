5891dcf75bdb18afb54dbc2259d8bfe7bcbe3761
knowbodykim <knowbodykim@gmail.com>
2024-12-06 14:15:25 +0900

    Add sub, mul, div, pow function in ctrl.py

--- ui.py
+++ ui.py
@@ -27,7 +27,7 @@ class View(QWidget):
         self.le2.setAlignment(QtCore.Qt.AlignRight)
         
         self.cb = QComboBox(self)
-        self.cb.addItems(['+', '-', '*', '/'])
+        self.cb.addItems(['+', '-', '*', '/', '^'])
         
         hbox_formular = QHBoxLayout()
         hbox_formular.addWidget(self.le1)
@@ -52,7 +52,7 @@ class View(QWidget):
         self.resize(256,256)
         self.show()
         
-    def activateMessage(self, text):
+    def setDisplay(self, text):
         self.te1.appendPlainText(text)
         
     def clearMessage(self):
